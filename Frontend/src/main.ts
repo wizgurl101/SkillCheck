996ad1d17c9852cfcd7c001b4cd25e97743a1f59
wizgurl101 <nb.phan@hotmail.com>
2026-08-15 14:54:29 -0600

    update css style

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -9,6 +9,22 @@ import * as directives from "vuetify/directives";
 const vuetify = createVuetify({
   components,
   directives,
+
+  theme: {
+    defaultTheme: "skillCheckTheme",
+
+    themes: {
+      skillCheckTheme: {
+        colors: {
+          background: "#1D546D",
+          surface: "#F3F4F4",
+          primary: "#061E29",
+          "on-background": "#061E29",
+          "on-surface": "#061E29",
+        },
+      },
+    },
+  },
 });
 
 createApp(App).use(vuetify).mount("#app");
